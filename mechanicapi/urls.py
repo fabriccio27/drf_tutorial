ad5a96895d34d9669dc36fd7ed261ec57fcf8d2d
fabriccio27 <fabricio_inc27@hotmail.com>
2022-10-31 10:34:07 -0300

    Add built in view

--- mechanicapi/urls.py
+++ mechanicapi/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('customers.urls')),
     path('', include('vehicles.urls')),
+    path('api/api-token-auth', views.obtain_auth_token),
 ]
