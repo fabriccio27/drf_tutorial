97426c949909f4b1f46f2889ba09e20f648048cb
fabriccio27 <fabricio_inc27@hotmail.com>
2022-11-01 08:07:35 -0300

    Add permission class

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import VehicleSerializer
 from .models import Vehicle
 
 # Create your views here.
 class VehiclesList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = VehicleSerializer
     queryset = Vehicle.objects.all()
     
